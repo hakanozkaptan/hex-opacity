1f0e8be128d008a66a86ebacdaa8c9408299437b
Hakan Ozkaptan <ozkaptanhakan@gmail.com>
2020-01-14 11:16:18 +0300

    Hex Opacity

---
+++ README.md
@@ -0,0 +1,9 @@
+## Opacity for hex value
+
+```javascript
+ rgba(0, 0, 0, 0.7) => #000000B3
+```
+
+```javascript
+ rgba(0, 0, 0, 0.9) => #000000E6
+```
---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import './App.css';
+
+const App = () => {
+  const [value, setValue] = useState('');
+  const [hexValue, setHexValue] = useState(null);
+
+  const percentToHex = p => {
+    if (p > 100 || p < 0) {
+      p = 100;
+    }
+
+    const intValue = Math.round((p / 100) * 255);
+    const hex = intValue.toString(16);
+    return hex.padStart(2, '0').toUpperCase();
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    setHexValue(percentToHex(value));
+    setValue('');
+  };
+
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <form onSubmit={e => handleSubmit(e)}>
+          <input
+            min='0'
+            max='100'
+            step='1'
+            className='App-input'
+            value={value}
+            placeholder='0-100'
+            onChange={event => {
+              setValue(event.target.value);
+            }}
+          />
+          <button className='App-button' type='submit'>
+            Convert To Hex
+          </button>
+        </form>
+        <div className='App-value'>{hexValue && hexValue}</div>
+      </header>
+    </div>
+  );
+};
+
+export default App;
