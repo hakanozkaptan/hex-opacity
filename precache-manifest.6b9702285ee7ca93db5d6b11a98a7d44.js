0ce6989e444aca0f7ed5970dfff508d11347f8d8
Hakan Ozkaptan <ozkaptanhakan@gmail.com>
2020-01-14 11:22:45 +0300

    Updates

---
+++ precache-manifest.6b9702285ee7ca93db5d6b11a98a7d44.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0114a19d663d5671bc9869231d19249c",
+    "url": "/hex-opacity/index.html"
+  },
+  {
+    "revision": "49709bbe0a57d935dc2b",
+    "url": "/hex-opacity/static/css/main.cd692c0e.chunk.css"
+  },
+  {
+    "revision": "0cdc1aff054ee46b5218",
+    "url": "/hex-opacity/static/js/2.7f880749.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/hex-opacity/static/js/2.7f880749.chunk.js.LICENSE"
+  },
+  {
+    "revision": "49709bbe0a57d935dc2b",
+    "url": "/hex-opacity/static/js/main.87935b45.chunk.js"
+  },
+  {
+    "revision": "941510de40a1f3a4f6c1",
+    "url": "/hex-opacity/static/js/runtime-main.c373f7d4.js"
+  }
+]);
